4edbab78dee5734ae4f0b09ba1b326045b80eba1
Nishant Pandey <nishantpandey2106@gmail.com>
2021-08-07 13:39:41 +0530

    Class Based Setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import One from './Component/One';
+import Two from './Component/Two';
+import Three from './Component/Three';
+import Four from './Component/Four';
+import Five from './Component/Five';
+
+function App() {
+  return (
+    <div className="App">
+      <One/>
+      <Two/>
+      <Three/>
+      <Four/>
+      <Five/>
+    </div>
+  );
+}
+
+export default App;
